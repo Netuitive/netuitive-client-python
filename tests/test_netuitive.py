35c58ae9f6ef71b0c651e0320ab5329c99776509
Shawn Butts <me@shawnbutts.com>
2015-06-12 08:22:28 -0400

    add more tests

--- tests/test_netuitive.py
+++ tests/test_netuitive.py
@@ -43,5 +43,60 @@ class TestElementInit(unittest.TestCase):
         pass
 
 
+class TestElementAttributes(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        a = netuitive.Element()
+        a.add_attribute('Test', 'TestValue')
+        self.failUnlessEqual(a.attributes[0].name, 'Test')
+        self.failUnlessEqual(a.attributes[0].value, 'TestValue')
+
+    def tearDown(self):
+        pass
+
+
+class TestElementTags(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        a = netuitive.Element()
+        a.add_tag('Test', 'TestValue')
+
+        self.failUnlessEqual(a.tags[0].name, 'Test')
+        self.failUnlessEqual(a.tags[0].value, 'TestValue')
+
+    def tearDown(self):
+        pass
+
+
+class TestElementSamples(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        a = netuitive.Element()
+        a.add_sample(
+            'metricId', 1434110794, 1, 'COUNTER', host='hostname')
+
+        self.failUnlessEqual(a.id, 'hostname')
+        self.failUnlessEqual(a.name, 'hostname')
+
+        self.failUnlessEqual(a.metrics[0].id, 'metricId')
+        self.failUnlessEqual(a.metrics[0].type, 'COUNTER')
+
+        self.failUnlessEqual(a.samples[0].metricId, 'metricId')
+        self.failUnlessEqual(a.samples[0].timestamp, 1434110794000)
+        self.failUnlessEqual(a.samples[0].val, 1)
+
+    def tearDown(self):
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
