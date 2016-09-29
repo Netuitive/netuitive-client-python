03c98b4d5f6e14e332cd4273445fd5f5e10471d0
shawnbutts <shawnbutts@users.noreply.github.com>
2016-09-29 14:17:27 -0400

    fix lint issue with latest flake8

--- netuitive/__init__.py
+++ netuitive/__init__.py
@@ -3,7 +3,7 @@
 __author__ = 'Netuitive, Inc'
 __version__ = '0.2.0'
 
-from .client import Client  # nopep8
-from .element import Element  # nopep8
-from .event import Event  # nopep8
-from .util import to_ms_timestamp  # nopep8
+from .client import Client  # nopep8 # flake8: noqa
+from .element import Element  # nopep8 # flake8: noqa
+from .event import Event  # nopep8 # flake8: noqa
+from .util import to_ms_timestamp  # nopep8 # flake8: noqa
