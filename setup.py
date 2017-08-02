cb4e296505368781f2518aafa6b5e80d1ae04e67
TheConnMan <bcconn2112@gmail.com>
2017-08-02 13:27:12 -0400

    Version bump to v0.2.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='netuitive',
-    version='0.2.3',
+    version='0.2.4',
     description="Python Client for Netuitive Cloud",
     long_description=readme + '\n\n' + history,
     author="Netuitive",
