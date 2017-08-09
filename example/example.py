59dc6605af2aba9c94201b5b08e614015c8824dc
rclag1126 <rclag1126@gmail.com>
2017-08-09 15:57:57 -0400

    Use localtime function

--- example/example.py
+++ example/example.py
@@ -14,7 +14,7 @@ MyElement.add_relation('my_child_element')
 MyElement.add_tag('Production', 'True')
 MyElement.add_tag('app_tier', 'True')
 
-timestamp = int(time.mktime(time.gmtime()))
+timestamp = int(time.mktime(time.localtime()))
 MyElement.add_sample('app.error', timestamp, 1, host='appserver01')
 MyElement.add_sample('app.request', timestamp, 10, host='appserver01')
 
