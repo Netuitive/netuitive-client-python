2db4ecf617a483ec6c66a1fced7e9a3ce6804afb
Yahong Gu <yahong.gu@netuitive.com>
2018-02-16 15:07:40 -0500

    Version bump to 0.3.2

--- netuitive/__init__.py
+++ netuitive/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Netuitive, Inc'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from .client import Client  # nopep8 # flake8: noqa
 from .element import Element  # nopep8 # flake8: noqa
